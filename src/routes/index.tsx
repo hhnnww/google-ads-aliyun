9b8d49a259dd1ec15f12e51be509e057845ddcdc
wuweihua <175990271@qq.com>
2026-08-16 14:50:38 +0800

    123123123

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { Button } from "#/components/ui/button.tsx";
 import { imageApi } from "#/features/image/api.ts";
 
 export const Route = createFileRoute("/")({
@@ -12,13 +11,9 @@ function RouteComponent() {
 		imageApi.list.queryOptions({ input: { pageNum: 1, pageSize: 10 } }),
 	);
 	return (
-		<div className="p-10 flex flex-col gap-10 items-start">
-			<Button>西藏域龙旅行社</Button>
-			<img
-				src="/uploads/2026/8/1786856459819-8123495.webp"
-				alt="西藏域龙旅行社"
-			/>
-			{JSON.stringify(imageListQuery.data)}
+		<div className="container mx-auto py-20 flex flex-col gap-20 items-start">
+			<div className="">tibet travel</div>
+			<pre className="">{JSON.stringify(imageListQuery.data, null, 4)}</pre>
 		</div>
 	);
 }
