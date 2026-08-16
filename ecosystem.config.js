d1651df2459e4c8e3d29c2bf13baef9ac7fba897
wuweihua <175990271@qq.com>
2026-08-16 12:11:16 +0800

    123123123

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "tibet-travel-english",       // 你的项目名称
-      script: ".output/server/index.mjs", // TanStack Start 的生产构建入口
+      script: "./server/index.mjs", // TanStack Start 的生产构建入口
       instances: 1,                       // 或者 'max' 利用多核
       exec_mode: "cluster",               // 开启集群模式
       env: {
