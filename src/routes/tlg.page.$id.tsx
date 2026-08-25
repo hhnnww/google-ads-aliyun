1aa4ac0f23f4adfc0bc77498c9ec6b85f6ecce14
wuweihua <175990271@qq.com>
2026-08-25 12:23:05 +0800

    1231231231

--- src/routes/tlg.page.$id.tsx
+++ src/routes/tlg.page.$id.tsx
@@ -1,24 +1,24 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { FrontPage } from "#/features/travelogue/front/components/page.tsx";
+import { orpc } from "#/orpc/client.ts";
 
 export const Route = createFileRoute("/tlg/page/$id")({
 	component: RouteComponent,
-	// loader: async (ctx) => {
-	// 	return orpc.frontRouter.get.call({ pageId: Number(ctx.params.id) });
-	// },
+	loader: async (ctx) => {
+		return orpc.frontRouter.get.call({ pageId: Number(ctx.params.id) });
+	},
 
-	// head: (ctx) => ({
-	// 	meta: [
-	// 		{
-	// 			title: `${ctx.loaderData?.page?.title} - 西藏域龍旅行社` || "",
-	// 		},
-	// 	],
-	// }),
+	head: (ctx) => ({
+		meta: [
+			{
+				title: `${ctx.loaderData?.page?.title} - 西藏域龍旅行社` || "",
+			},
+		],
+	}),
 });
 
 function RouteComponent() {
 	const page = Route.useLoaderData();
 
-	// return <FrontPage page={page} />;
-
-	return <pre className="">fuck you</pre>;
+	return <FrontPage page={page} />;
 }
